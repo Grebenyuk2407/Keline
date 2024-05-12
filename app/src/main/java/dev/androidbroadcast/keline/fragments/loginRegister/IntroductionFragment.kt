08f984a22c1c742f3539c14d23612c44a4db38be
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-05-12 14:02:01 +0300

    finish introductio fragment xml

---
+++ app/src/main/java/dev/androidbroadcast/keline/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package dev.androidbroadcast.keline.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import dev.androidbroadcast.keline.R
+
+class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
+}
---
+++ app/src/main/java/dev/androidbroadcast/keline/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package dev.androidbroadcast.keline.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import dev.androidbroadcast.keline.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+}
