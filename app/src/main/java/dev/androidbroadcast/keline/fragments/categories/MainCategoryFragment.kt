610fad596503d44c9338820480b2d0b9286092fd
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-05-18 16:21:25 +0300

    add HomeViewpagerAdapter

--- app/src/main/java/dev/androidbroadcast/keline/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/dev/androidbroadcast/keline/fragments/categories/MainCategoryFragment.kt
@@ -3,5 +3,5 @@ package dev.androidbroadcast.keline.fragments.categories
 import androidx.fragment.app.Fragment
 import dev.androidbroadcast.keline.R
 
-class MainCategory : Fragment(R.layout.fragment_main_category) {
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
 }
--- app/src/main/java/dev/androidbroadcast/keline/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/dev/androidbroadcast/keline/fragments/shopping/HomeFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import dev.androidbroadcast.keline.R
+import dev.androidbroadcast.keline.adapters.HomeViewpagerAdapter
 import dev.androidbroadcast.keline.databinding.FragmentHomeBinding
+import dev.androidbroadcast.keline.fragments.categories.AccessoryFragment
+import dev.androidbroadcast.keline.fragments.categories.ChairFragment
+import dev.androidbroadcast.keline.fragments.categories.CupboardFragment
+import dev.androidbroadcast.keline.fragments.categories.FurnitureFragment
+import dev.androidbroadcast.keline.fragments.categories.MainCategoryFragment
+import dev.androidbroadcast.keline.fragments.categories.TableFragment
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
@@ -24,7 +32,26 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         val categoriesFragments = arrayListOf<Fragment>(
-
+            MainCategoryFragment(),
+            ChairFragment(),
+            CupboardFragment(),
+            TableFragment(),
+            AccessoryFragment(),
+            FurnitureFragment()
         )
+
+        val viewPager2Adapter =
+            HomeViewpagerAdapter(categoriesFragments, childFragmentManager, lifecycle)
+        binding.viewpagerHome.adapter = viewPager2Adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewpagerHome) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Main"
+                1 -> tab.text = "Chair"
+                2 -> tab.text = "Cupboard"
+                3 -> tab.text = "Table"
+                4 -> tab.text = "Accessory"
+                5 -> tab.text = "Furniture"
+            }
+        }.attach()
     }
 }
